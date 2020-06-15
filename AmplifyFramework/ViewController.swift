25897adaaa187f020eae4cf92588e2c36bfc29b7
Zeeshan Anjum <anjuz@amazon.com>
2020-06-15 15:13:26 +0100

    Initial Commit

---
+++ AmplifyFramework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmplifyFramework
+//
+//  Created by Anjum, Zeeshan on 15/06/2020.
+//  Copyright © 2020 Anjum, Zeeshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
